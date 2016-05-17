0ae6994f02476e17f9fc4749ae86a90d26048061
BuildTools <unconfigured@null.spigotmc.org>
2016-05-17 13:02:13 -0400

    should resolve conflicts. Added traps to shell script in inithooks.

--- overlay/usr/lib/inithooks/bin/mattermost_user.sh
+++ overlay/usr/lib/inithooks/bin/mattermost_user.sh
@@ -6,42 +6,36 @@ password="$2"
 email="$3"
 teamname="$4"
 
-<<<<<<< HEAD
-
-lines=<<EOF
-$platform/platform -create_user -team_name="$teamname" -email="$email" -pasword="$password" --username="username"
-$platform/platform -assign_role -email="$email" role="system_admin"
-EOF
-echo $lines
 
 
-###Saved just in case
-#$platform/platform -create_user -email="$email" -password="$password" -$
-#$platform/platform -create_team -team_name="$teamname" -email="$email"
-=======
+
+
+
+
 function create ()
 {
-	cat << EOF	
-$platform/platform -create_user -email="$email" -password="$password" -username="$username"
-EOF
-	cd $platform && ./platform -create_user -email="$email" -password="$password" -username="$username"
 
+	cd $platform && ./platform -create_user -email="$email" -password="$password" -username="$username"
 }
 function assign ()
 {
-	cat << EOF
-$platform/platform -assign_role -email="$email" -role="system_admin"
-EOF
 	cd $platform && ./platform -assign_role -email="$email" -role="system_admin"
-
 }
 
 if [ -e "$platform/platform" ]; then
 	create $platform $email $password $username
+	if [ $? > 0]; then
+		echo "create-user returned an error."
+		exit 1
+	fi
+
 	assign $platform $email
+	if [ $? > 0 ]; then
+		echo "assign-role failed."
+		exit 1
+	fi
 	exit 0
 else:
-	echo "There was a problem"
+	echo "The executable /opt/mattermost/bin/platform was not found."
 	exit 1
 fi
->>>>>>> 3c036c0ccae8ffc5faf6d9f402fdbd5362de702c
